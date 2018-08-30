5fe453ba93736e335fb1e83fcd9cc0f5a0f8ba27
yangzz <1160951574@qq.com>
2018-08-30 18:28:26 +0800

    初始化

---
+++ src/main/java/top/yangzhenzhong/StudySpringbootApplication.java
@@ -0,0 +1,12 @@
+package top.yangzhenzhong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudySpringbootApplication.class, args);
+	}
+}
---
+++ src/test/java/top/yangzhenzhong/StudySpringbootApplicationTests.java
@@ -0,0 +1,16 @@
+package top.yangzhenzhong;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StudySpringbootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
